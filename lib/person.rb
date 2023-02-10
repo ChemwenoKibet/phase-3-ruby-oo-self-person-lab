887efbcd7f423f3dd384b7d6b3baccd0f1abe90f
Ian <chemwenokibet11@gmail.com>
2023-02-10 23:57:09 +0300

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -37,7 +37,7 @@ class Person
     end
     
     def hygiene
-       @hygiene
+       @hygiene 
     end
 
     def happy?
@@ -71,7 +71,7 @@ class Person
 
     def take_bath
         self.hygiene= @hygiene + 4
-        #@hygiene += self.hygiene
+        
         return "♪ Rub-a-dub just relaxing in the tub ♫"
     end
 
@@ -81,10 +81,10 @@ class Person
         return "♪ another one bites the dust ♫"
     end
 
-    def call_friend(friend)
+    def call_friend(bredrin)
         self.happiness= @happiness + 3
-        friend.happiness= friend.happiness + 3
-        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+        bredrin.happiness= bredrin.happiness + 3
+        return "Hi #{bredrin.name}! It's #{self.name}. How are you?"
     end
 
     def start_conversation(person, topic)
@@ -103,3 +103,22 @@ class Person
     end
 
 end
+
+
+
+
+# The code is a Ruby class for representing a person. The class Person has the following attributes:
+
+# name: This is a reader attribute that returns the name of the person.
+# bank_account: This is an accessor attribute that can be used to read and write the value of the person's bank account.
+# The class also has the following methods:
+
+# initialize: This is a constructor method that is called when a new instance of the class is created. It sets the initial values for the person's name, bank account, happiness, and hygiene.
+# happiness and hygiene: These are accessor methods that can be used to read and write the values for the person's happiness and hygiene. They enforce constraints such that the values for happiness and hygiene must be between 0 and 10 inclusive.
+# happy? and clean?: These are methods that check the values of happiness and hygiene and return true if the value is greater than 7, or false otherwise.
+# get_paid: This is a method that takes a salary argument and increases the person's bank account by that amount.
+# take_bath: This is a method that increases the person's hygiene by 4.
+# work_out: This is a method that increases the person's happiness by 2 and decreases their hygiene by 3.
+# call_friend: This is a method that takes a friend object as an argument and increases the happiness of both the person and their friend by 3.
+# start_conversation: This is a method that takes another person and a topic as arguments. If the topic is "politics", it decreases the happiness of both people by 2. If the topic is "weather", it increases the happiness of both people by 1. If the topic is something else, it returns a generic string "blah blah blah blah blah".
+# The code also includes the line require 'pry' at the top. This is a library for debugging Ruby code and can be used to pause the execution of a program and examine the state of the program at that point.
